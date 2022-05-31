629bf0df20d8b98488cacaac91513d4ec86de91d
theGUI001 <lghmelo@gmail.com>
2022-05-31 15:16:48 -0300

    questão 7

---
+++ 7.py
@@ -0,0 +1,17 @@
+class Personagem:
+    def __init__(self, vocacao, nome, pontos_vida):
+        self.vocacao = vocacao
+        self.nome = nome
+        self.nivel = 1
+        self.pontos_vida = pontos_vida
+        self.dano_base = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+    def golpear_jogador(self, valor_golpe):
+        self.pontos_vida -= valor_golpe
+        print (f"O {self.nome} recebeu o dano de {valor_golpe} e sua vida atual é de {self.pontos_vida}")
+    
+    def __str__(self):
+        return f"DADOS DO PERSONAGEM:\n Nome: {self.nome}\n Pontos de Vida: {self.pontos_vida}\n Nível: {self.nivel}\n Vocação: {self.vocacao}"
+
+    def equipar_escudo(self):
+        self.pontos_vida += 20
